47e2d7e0b537f14a1cb9152ac57f6824d2973dfa
Gleb <rossevgleb@gmail.com>
2021-09-10 21:20:23 +0300

    add postcss and scss

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.js$/,
